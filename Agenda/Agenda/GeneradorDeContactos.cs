f332076a95988ae4969d0d4fa49f4f11e4633a69
Alexandro Munera <alexandromunera@gmail.com>
2017-01-10 16:09:30 -0500

    Fist Commit - Maester  y detalle con contactos aleatorios

---
+++ Agenda/Agenda/GeneradorDeContactos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda
+{
+    class GeneradorDeContactos
+    {
+        private static List<string> ListaNombres = new List<string>
+        {
+            "Alexandro", "Emanuel","Wilson","Cristina","Pedro"
+        };
+
+        private static List<string> ListaApellidos = new List<string>
+        {
+            "Múnera", "González","Tobón","Sifuentes","Mira"
+        };
+
+        public static ObservableCollection<Contacto> CrearContactos()
+        {
+            var random = new Random();
+            var contactos = new ObservableCollection<Contacto>();
+            for (int i = 0; i < 25; i++)
+            {
+                var nombre = ListaNombres[random.Next(ListaNombres.Count - 1)];
+                var apellidos = ListaApellidos[random.Next(ListaApellidos.Count - 1)];
+                var calle = ListaNombres[random.Next(ListaNombres.Count - 1)];
+                var contacto = new Contacto
+                {
+                    Nombre = nombre + " " + apellidos,
+                    Telefono = GenerarTelefono(),
+                    Direccion = "Avenida " + calle + ", Número " + random.Next(1, 1000)
+                };
+
+                contactos.Add(contacto);
+            }
+
+            return contactos;
+        }
+
+        private static string GenerarTelefono()
+        {
+            var random = new Random();
+            StringBuilder telefono = new StringBuilder();
+            telefono.Append("(");
+            telefono.Append(random.Next(100, 999));
+            telefono.Append(")");
+            telefono.Append(random.Next(1000000, 9999999));
+            return telefono.ToString();
+        }
+    }
+}
---
+++ Agenda/Agenda/PaginaListaContactos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Agenda
+{
+    public class PaginaListaContactos : MasterDetailPage
+    {
+        public PaginaListaContactos()
+        {
+            var lista = new ListView();
+            lista.ItemsSource = GeneradorDeContactos.CrearContactos().OrderBy(p => p.Nombre);
+
+            lista.ItemSelected += (sender, e) =>
+            {
+                if (e.SelectedItem != null)
+                {
+                    Detail = new PaginaContacto(e.SelectedItem as Contacto);
+                    IsPresented = false;
+                }
+            };
+
+            Master = new ContentPage
+            {
+                Title = "Contactos",
+                Content = lista
+            };
+
+            Detail = new PaginaContacto(GeneradorDeContactos.CrearContactos().First());
+
+            
+        }
+    }
+}
